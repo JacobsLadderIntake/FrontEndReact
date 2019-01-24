5beedcbe35944178d117970898036f1d50fe9bdf
emmadoksansky <emma.doksansky@gmail.com>
2019-01-24 11:59:58 -0500

    created parent table and header for the parent home page - errors exist

---
+++ src/Parent-Home/ParentTableHeader.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './header.css';
+
+class ParentTableHeader extends Component {
+    render() {
+        return (
+            <div classname="parent-table-header">
+                <h2>Intake Profile Checklist: Parent Name</h2>
+            </div>
+        );
+    }
+}
