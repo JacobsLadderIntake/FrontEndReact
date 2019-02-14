1a60d5b83a74f08406a0ae49f333d693af8db09e
Renee Botyrius <renee.bot30@gmail.com>
2019-02-13 20:34:55 -0500

    add error handling for login including checking username and password against dummy for now

--- src/Login.js
+++ src/Login.js
@@ -4,70 +4,76 @@ import './login.css'
 
 
 import {
-  Alert,
   Button,
   FormGroup,
-  InputGroup,
   Input,
-  Label
+  Label,
+  FormFeedback
 } from "reactstrap";
 
 
 class Login extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      errors: [],
-      fields: [],
-      email: "",
-      password: "",
-      startsHidden:true
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+          errors: [],
+          fields: [],
+          email: "test@test.com",
+          password: "test",
+          startsHidden:true,
+          loginButtonPressed:false
+        };
 
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-handleSubmit(e) {
-    e.preventDefault();
-
-    const password = ReactDOM.findDOMNode(this.password).value;
-    const email = ReactDOM.findDOMNode(this.email).value;
-    var errors = this.validate(email, password);
-    const isAdmin = this.isAdmin(email);
-
-    if (errors.length > 0) {
-      this.setState({ errors });
-      return;
-    } else {
-      if (isAdmin) {
-        this.props.history.push("/adminhome");
-      } else {
-        this.props.history.push("/parenthome");
-      }    
+        this.handleLogin = this.handleLogin.bind(this);
     }
-  }
 
-    validate() {
+    handleLogin(e) {
+        e.preventDefault();
+        this.state.loginButtonPressed = true;
+
+        const password = ReactDOM.findDOMNode(this.password).value;
+        const email = ReactDOM.findDOMNode(this.email).value;
+
+        if(this.validate(email, password) && this.isAdmin()) {
+            this.props.history.push("/adminhome");
+        } else if (this.validate()){
+
+        } else {
+            return
+        }
+    }
+
+    handleChange(field, e) {
+        let fields = this.state.fields;
+        fields[field] = e.target.value;
+        this.validate()
+        this.setState({fields});
+    }
+
+    validate(email, password) {
         // we are going to store errors for all fields
         // in a single array
         let fields = this.state.fields;
         let errors = {};
         let formIsValid = true;
-        if(this.state.submitButtonHit) {
+        if(this.state.loginButtonPressed) {
+            if (email !== this.state.email && password !== this.state.password ) {
+                errors["email"] = "Email or password is incorrect"
+                errors["password"] = "Email or password is incorrect"
+                formIsValid = false;
+            }
             if (!fields["email"]) {
                 formIsValid = false;
                 errors["email"] = "Cannot be empty";
 
-            }
-            if (fields["email"] && (fields["email"].indexOf("@") === -1 || fields["email"].indexOf(".") === -1)) {
-                errors["email"] = "Email is not formatted correctly"
             }
 
             if (!fields["password"]) {
                 formIsValid = false;
                 errors["password"] = "Cannot be empty";
             }
+
         }
 
         this.setState({errors: errors})
@@ -78,42 +84,35 @@ handleSubmit(e) {
     return true;
   }
 
-
-  handleChange (event) {
-    let change = {}
-    change[event.target.id] = event.target.value
-    this.setState(change)
-
-  }
-
   renderForm() {
     const { errors } = this.state;
 
     return (
-        <form className="form-style" onSubmit={this.handleSubmit}>
+        <form className="form-style" onSubmit={this.handleLogin}>
             <div className="login-page-title">
                 <h1> Existing User Login</h1>
             </div>
             <div className = "question-fields">
-                {errors.map(error => (
-                    <Alert color = "warning" key={error}>Error: {error}</Alert>))}
-                    <FormGroup check/>
-                <InputGroup id="email">
+                <FormGroup id="email">
                     <Label className="control-label required" lg={6}>Email</Label>
                     <Input lg={12}
                         ref={email => (this.email = email)}
                         type="text"
                         value={this.state.id}
-                        onChange={event => this.handleChange(event)}/>
-                </InputGroup>
-                <InputGroup id="password">
+                        onChange={this.handleChange.bind(this, "email")}
+                        invalid={this.state.errors["email"]}/>
+                    <FormFeedback invalid = {this.state.errors["email"]}>{this.state.errors["email"]}</FormFeedback>
+                </FormGroup>
+                <FormGroup id="password">
                     <Label className="control-label required" sm={6}>Password</Label>
                     <Input sm={12}
                         value={this.state.id}
                         ref={password => (this.password = password)}
-                        onChange={event => this.handleChange(event)}
-                        type="password"/>
-                </InputGroup>
+                        onChange={this.handleChange.bind(this, "password")}
+                        type="password"
+                        invalid={this.state.errors["password"]}/>
+                    <FormFeedback invalid = {this.state.errors["password"]}>{this.state.errors["password"]}</FormFeedback>
+                </FormGroup>
                 <div className="button-div">
                     <Button onClick={this.validForm}
                             color="success"
