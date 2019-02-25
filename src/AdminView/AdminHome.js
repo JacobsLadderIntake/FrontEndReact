2eaf556897b8b0a76b1ca47dfdea691146564105
Renee Botyrius <renee.bot30@gmail.com>
2019-02-25 10:28:11 -0500

    make connection with the database to fill in admin name on the home screen

--- src/AdminView/AdminHome.js
+++ src/AdminView/AdminHome.js
@@ -10,10 +10,36 @@ class AdminHome extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            user: "admin name"
+            user: "",
         };
 
     }
+
+    componentDidMount() {
+        this.callApi()
+            .then(res => this.setState({ response: res.express }))
+            .catch(err => console.log(err));
+    }
+    callApi = async () => {
+        const response = await fetch('/Users/Emma@gmail.com');
+        const body = await response.json();
+        console.log(body);
+        this.state.user = body.UserFirstName;
+        if (response.status !== 200) throw Error(body.message);
+        return body;
+    };
+    // handleSubmit = async e => {
+    //     e.preventDefault();
+    //     const response = await fetch('/api/world', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //         },
+    //         body: JSON.stringify({ post: this.state.post }),
+    //     });
+    //     const body = await response.text();
+    //     this.setState({ responseToPost: body });
+    // };
 
     createCards() {
         let cards = [];
